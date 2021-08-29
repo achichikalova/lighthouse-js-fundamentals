4dab25f9b192bd0a58eafa8ca0bceeba303b570e
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-08-29 21:11:36 +0000

    Move files to kata directory

--- urlEncode.js
+++
@@ -1,7 +0,0 @@
-const urlEncode = function(text) {
-  return text.trim().replace(/ /gi, "%20");
-};
-
-console.log(urlEncode("Lighthouse Labs"));
-console.log(urlEncode(" Lighthouse Labs "));
-console.log(urlEncode("blue is greener than purple for sure"));
